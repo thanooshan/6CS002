9f703136ee59751dabbd6f3f6666718ebf62b31c
thanooshan <gtshan10@gmail.com>
2016-06-12 13:16:49 +0530

    initial commit

---
+++ src/mohammed012/IOSpecialist.java
@@ -0,0 +1,14 @@
+package mohammed012;
+
+/**
+ * @author Kevan Buckley, maintained by Amjad Mohammed
+ * @version 2.0, 2014
+ */
+
+public class IOSpecialist {
+  public IOSpecialist() {   
+  }
+  public String getString(){
+    return IOLibrary.getString();
+  }
+}
---
+++ src/mohammed012/Portishead.java
@@ -0,0 +1,14 @@
+package mohammed012;
+public class Portishead {
+  static transient String[] wildcat = {
+    "A scientific man ought to have no wishes, no affections, -- a mere heart of stone.","Charles Darwin","",
+    "He was as great as a man can be without morality.","Alexis de Tocqueville","",
+    "Mathematics is the language with which God has written the universe.","Galileo Galilei","",
+    "Experience proves this, or that, or nothing, according to the preconceptions we bring to it.","C.S. Lewis","",
+    "A man never lies as much as after a hunt, during a war, and before an election.","Otto von Bismarck","",
+    "There is no cosmetic for beauty like happiness.","Marguerite Gardiner Blessington","",
+    "Experience is not what happens to a man; it is what a man does with what happens to him.","Aldous Huxley","",
+    "Every great advance in science has issued from a new audacity of the imagination.","John Dewey","",
+    "As soon as one is unhappy one becomes moral.","Marcel Proust","",
+    "Beauty is truth, truth beauty. That is all ye know on Earth, and all ye need to know.","John Keats",""};
+}
